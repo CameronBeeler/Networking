226cec5b27634c94f53da352fa27802ceb16d43a
Cameron Beeler <cameron.beeler@gmail.com>
2019-06-04 22:00:21 -0700

    adding baseuri and joining with relative uris

--- src/com/cambeeler/Main.java
+++ src/com/cambeeler/Main.java
@@ -13,11 +13,20 @@ public class Main {
 //            URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
             URI baseURI = new URI("http://username:password@myserver.com:5000"); // can be managed in 1 place and
             // all relative uri's can be handled separately
-            URI uri = new URI("/catalogue/phones?os=android#samsung");
-            URI resolvedURI = baseURI.resolve(uri);
-            URL url = resolvedURI.toURL();  // then all combined into a single complete URI to be converted into URL's as needed.
-            System.out.println("URL: " + url);
+            URI uri1 = new URI("/catalogue/phones?os=android#samsung");
+            URI uri2 = new URI("/catalogue/TVs?manufacturing=samsung");
+            URI uri3 = new URI("/catalogue/locations?zip=12345");
+            URI resolvedURI = baseURI.resolve(uri1);
+            URL url1 = resolvedURI.toURL();  // then all combined into a single complete URI to be converted into URL's as needed.
+            resolvedURI = baseURI.resolve(uri2);
+            URL url2 = resolvedURI.toURL();  // then all combined into a single complete URI to be converted into URL's as needed.
+            resolvedURI = baseURI.resolve(uri3);
+            URL url3 = resolvedURI.toURL();  // then all combined into a single complete URI to be converted into URL's as needed.
+            System.out.println("URL: " + url1);
+            System.out.println("URL: " + url2);
+            System.out.println("URL: " + url3);
 
+/*
             System.out.println("scheme: " + uri.getScheme());
             System.out.println("scheme-specific-part: " + uri.getSchemeSpecificPart());
             System.out.println("authority: " + uri.getAuthority());
@@ -27,7 +36,7 @@ public class Main {
             System.out.println("path: " + uri.getPath());
             System.out.println("query: " + uri.getQuery());
             System.out.println("fragment: " + uri.getFragment());
-
+ */
         }
         catch (URISyntaxException u)
         {
